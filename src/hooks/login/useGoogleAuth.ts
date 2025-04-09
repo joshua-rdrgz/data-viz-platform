eb7829077eb089643ddb76f38bdb7bac01202c94
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2025-04-09 18:49:25 -0500

    Refactor Google authentication logic
    - Extract the authentication function into a separate async function for improved readability and maintainability.

--- src/hooks/login/useGoogleAuth.ts
+++ src/hooks/login/useGoogleAuth.ts
@@ -1,23 +1,21 @@
 import { firebaseAuth } from '@/config/firebase';
 import { queryKeys } from '@/lib/query-keys';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import {
-  GoogleAuthProvider,
-  signInWithPopup,
-  UserCredential,
-} from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+
+async function performGoogleAuth() {
+  const googleAuthProvider = new GoogleAuthProvider();
+  return signInWithPopup(firebaseAuth, googleAuthProvider);
+}
 
 export const useGoogleAuth = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   return useMutation({
     mutationKey: queryKeys.auth.google,
-    mutationFn: async (): Promise<UserCredential> => {
-      const googleAuthProvider = new GoogleAuthProvider();
-      return signInWithPopup(firebaseAuth, googleAuthProvider);
-    },
+    mutationFn: performGoogleAuth,
     onSuccess: (data) => {
       queryClient.setQueryData(queryKeys.auth.user, data);
       localStorage.setItem('user', JSON.stringify(data));
